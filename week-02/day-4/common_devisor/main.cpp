74f7b20fd24d8b4de8759144ea3698d4ca9f6edd
Szecsi Alex <szecsia.929@gmail.com>
2019-04-11 15:38:51 +0200

    Common devisor now works fine.

--- week-02/day-4/common_devisor/main.cpp
+++ week-02/day-4/common_devisor/main.cpp
@@ -4,10 +4,15 @@ int commonDevisor(int num1, int num2);
 
 int main() {
 
-    int num1, num2;
+    int num1, num2, swap;
 
     std::cout << "Give me two numbers! ";
     std::cin >> num1 >> num2;
+    if (num1 < num2) {
+        swap = num1;
+        num1 = num2;
+        num2 = swap;
+    }
 
     std::cout << commonDevisor(num1, num2);
 
@@ -18,6 +23,6 @@ int commonDevisor(int num1, int num2) {
     if (num1 >= num2 && num1 % num2 == 0) {
         return num2;
     } else {
-        return commonDevisor(num1, num2-1);
+        return commonDevisor(num1, num2 - 1);
     }
 }
