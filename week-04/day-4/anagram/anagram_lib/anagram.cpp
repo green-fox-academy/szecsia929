54699df477fb1ab60925e4d948fa7d188849da23
Szecsi Alex <szecsia.929@gmail.com>
2019-04-25 17:03:11 +0200

    with sort algorithm

--- week-04/day-4/anagram/anagram_lib/anagram.cpp
+++ week-04/day-4/anagram/anagram_lib/anagram.cpp
@@ -2,28 +2,12 @@
 // Created by user on 2019. 04. 25..
 //
 
-#include <string>
+#include <iostream>
+#include <algorithm>
 
 bool isAnagram(std::string text1, std::string text2)
 {
-    bool isAnagram = true;
-    int counter1 = 0, counter2 = 0;
-    if (text1.size() == text2.size()) {
-        for (int i = 0; i < text1.size(); ++i) {
-            for (int j = 0; j < text2.size(); ++j) {
-                if (text1.at(i) == text1.at(j)) {
-                    counter1++;
-                }
-                if (text1.at(i) == text2.at(j)) {
-                    counter2++;
-                }
-            }
-            if (counter1 != counter2) {
-                isAnagram = false;
-            }
-        }
-    } else {
-        isAnagram = false;
-    }
-    return isAnagram;
+    std::sort(text1.begin(), text1.end());
+    std::sort(text2.begin(), text2.end());
+    return text1 == text2;
 }
