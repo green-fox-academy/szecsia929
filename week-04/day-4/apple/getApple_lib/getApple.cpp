fdfc00ea6573845679479fa0dcb5a53b2bb3d510
Szecsi Alex <szecsia.929@gmail.com>
2019-04-25 14:23:37 +0200

    In order.

--- week-04/day-4/apple/getApple_lib/getApple.cpp
+++ week-04/day-4/apple/getApple_lib/getApple.cpp
@@ -3,9 +3,18 @@
 //
 
 #include <string>
+#include <vector>
 
 std::string getApple()
 {
     return "apple";
 }
 
+int sum(std::vector<int> numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.size(); ++i) {
+        sum += numbers.at(i);
+    }
+    return sum;
+}
--- week-04/day-4/apple/getApple_tests/getApple_tests.cpp
+++ week-04/day-4/apple/getApple_tests/getApple_tests.cpp
@@ -5,8 +5,37 @@
 #include "getApple.cpp"
 #include "gtest/gtest.h"
 
-TEST(getAppleTest, getAppleTest_1){
-    std::string test = "apple";
-    ASSERT_EQ(test, getApple());
+TEST(getAppleTest, getAppleTest_1)
+{
+    std::string apple = "apple";
+    std::string test = getApple();
+    ASSERT_EQ(apple, test);
 }
 
+TEST(sumTest, emptyList)
+{
+    std::vector<int> testVector;
+    int testSum = sum(testVector);
+    ASSERT_EQ(0, testSum);
+}
+
+TEST(sumTest, oneElement)
+{
+    std::vector<int> testVector = {1};
+    int testSum = sum(testVector);
+    ASSERT_EQ(1, testSum);
+}
+
+TEST(sumTest, multipleElements)
+{
+    std::vector<int> testVector = {1, 1, 1};
+    int testSum = sum(testVector);
+    ASSERT_EQ(3, testSum);
+}
+
+TEST(sumTest, negativeSum)
+{
+    std::vector<int> testVector = {-1, -1, -1};
+    int testSum = sum(testVector);
+    ASSERT_EQ(-3, testSum);
+}
