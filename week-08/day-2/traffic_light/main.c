f57484926c7fc9a09c78026901bd65a9d5e60c25
Szecsi Alex <szecsia.929@gmail.com>
2019-05-28 16:55:55 +0200

    traffic light

---
+++ week-08/day-2/traffic_light/main.c
@@ -0,0 +1,131 @@
+/**
+ ******************************************************************************
+ * @file    main.c
+ * @author  Ac6
+ * @version V1.0
+ * @date    01-December-2013
+ * @brief   Default main function.
+ ******************************************************************************
+ */
+
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+#include "stm32f7xx_hal_tim.h"
+
+static void SystemClock_Config(void);
+static void Error_Handler(void);
+
+TIM_HandleTypeDef timer_handle;
+GPIO_InitTypeDef led_pin;
+
+void init_timer()
+{
+	__HAL_RCC_TIM2_CLK_ENABLE()
+	;
+
+	timer_handle.Instance = TIM2;
+	timer_handle.Init.Prescaler = 54000 - 1;
+	timer_handle.Init.Period = 24000 - 1;
+	timer_handle.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+	timer_handle.Init.CounterMode = TIM_COUNTERMODE_UP;
+
+	HAL_TIM_Base_Init(&timer_handle);
+}
+
+void init_led_pin(void)
+{
+	__HAL_RCC_GPIOF_CLK_ENABLE()
+	;
+
+	led_pin.Pin = GPIO_PIN_10 | GPIO_PIN_9 | GPIO_PIN_8;
+	led_pin.Mode = GPIO_MODE_OUTPUT_PP;
+	led_pin.Pull = GPIO_NOPULL;
+	led_pin.Speed = GPIO_SPEED_FAST;
+	HAL_GPIO_Init(GPIOF, &led_pin);
+}
+
+int main(void)
+{
+	HAL_Init();
+
+	SystemClock_Config();
+
+	init_led_pin();
+	init_timer();
+
+	HAL_TIM_Base_Start(&timer_handle);
+
+	uint16_t timer_value = 0;
+
+	while (1) {
+
+		timer_value = __HAL_TIM_GET_COUNTER(&timer_handle);
+
+		if (timer_value == 0) {
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET);
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET);
+		}
+		if (timer_value == 5000) {
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET);
+		}
+		if (timer_value == 8000) {
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_SET);
+		}
+		if (timer_value == 16000) {
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET);
+		}
+		if (timer_value == 19000) {
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET);
+			HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET);
+		}
+	}
+}
+
+static void Error_Handler(void)
+{
+}
+
+static void SystemClock_Config(void)
+{
+	RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
+	RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+
+	/**Configure the main internal regulator output voltage */
+	__HAL_RCC_PWR_CLK_ENABLE()
+	;
+	__HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+	/**Initializes the CPU, AHB and APB busses clocks */
+	RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+	RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+	RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+	RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+	RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
+	RCC_OscInitStruct.PLL.PLLM = 8;
+	RCC_OscInitStruct.PLL.PLLN = 216;
+	RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+	RCC_OscInitStruct.PLL.PLLQ = 2;
+
+	if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK) {
+		Error_Handler();
+	}
+
+	/**Activate the Over-Drive mode */
+	if (HAL_PWREx_EnableOverDrive() != HAL_OK) {
+		Error_Handler();
+	}
+
+	/**Initializes the CPU, AHB and APB busses clocks */
+	RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+	RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+	RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+	RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+	RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+
+	if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK) {
+		Error_Handler();
+	}
+}
