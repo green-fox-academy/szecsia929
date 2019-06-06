4b6fe23a3896a9da6861f7cfa0bcf2db9fd60b68
Szecsi Alex <szecsia.929@gmail.com>
2019-06-06 14:56:36 +0200

    railroad crossing completed

--- week-09/orientation_exam_trial/railroad_crossing/main.c
+++ week-09/orientation_exam_trial/railroad_crossing/main.c
@@ -12,9 +12,11 @@
 #include "stm32746g_discovery.h"
 
 TIM_HandleTypeDef timer_handle;
+UART_HandleTypeDef uart_handle;
 
 void SystemClock_Config();
 void init_timer();
+void init_uart();
 
 volatile int time = 0;
 volatile int closed_mode = 0;
@@ -22,13 +24,21 @@ volatile int lowered = 0;
 volatile int button = 0;
 volatile int stance_changeing_timer = 0;
 
+volatile char open_state[] = "Entered open state.\n";
+volatile char opening_state[] = "Entered opening state.\n";
+volatile char securing_state[] = "Entered securing state.\n";
+volatile char secured_state[] = "Entered secured state.\n";
+
 int main(void)
 {
 	SystemClock_Config();
 	init_timer();
+	init_uart();
 	BSP_PB_Init(BUTTON_WAKEUP, BUTTON_MODE_EXTI);
 	BSP_LED_Init(LED1);
 	HAL_TIM_Base_Start_IT(&timer_handle);
+
+	HAL_UART_Transmit(&uart_handle, (uint8_t*) open_state, strlen(open_state), 0xFFFF);
 
 	while (1) {
 	}
@@ -61,6 +71,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 		button = 0;
 		lowered = 1;
 		stance_changeing_timer = 0;
+		HAL_UART_Transmit(&uart_handle, (uint8_t*) secured_state, strlen(secured_state), 0xFFFF);
 		BSP_PB_Init(BUTTON_WAKEUP, BUTTON_MODE_EXTI);
 	}
 
@@ -69,6 +80,7 @@ void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
 		lowered = 0;
 		closed_mode = 0;
 		stance_changeing_timer = 0;
+		HAL_UART_Transmit(&uart_handle, (uint8_t*) open_state, strlen(open_state), 0xFFFF);
 		BSP_PB_Init(BUTTON_WAKEUP, BUTTON_MODE_EXTI);
 	}
 
@@ -88,6 +100,13 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
 {
 	closed_mode = 1;
 	button = 1;
+
+	if (lowered) {
+		HAL_UART_Transmit(&uart_handle, (uint8_t*) opening_state, strlen(opening_state), 0xFFFF);
+	} else {
+		HAL_UART_Transmit(&uart_handle, (uint8_t*) securing_state, strlen(securing_state), 0xFFFF);
+	}
+
 	BSP_PB_DeInit(BUTTON_WAKEUP);
 }
 
@@ -144,4 +163,44 @@ void init_timer()
 
 	HAL_NVIC_SetPriority(TIM2_IRQn, 1, 0);
 	HAL_NVIC_EnableIRQ(TIM2_IRQn);
+}
+
+void init_uart()
+{
+	GPIO_InitTypeDef GPIOTx_handle;
+	GPIO_InitTypeDef GPIORx_handle;
+
+	__HAL_RCC_GPIOA_CLK_ENABLE()
+	;
+	__HAL_RCC_GPIOB_CLK_ENABLE()
+	;
+
+	GPIOTx_handle.Pin = GPIO_PIN_9;
+	GPIOTx_handle.Mode = GPIO_MODE_AF_PP;
+	GPIOTx_handle.Pull = GPIO_NOPULL;
+	GPIOTx_handle.Speed = GPIO_SPEED_FAST;
+	GPIOTx_handle.Alternate = GPIO_AF7_USART1;
+
+	HAL_GPIO_Init(GPIOA, &GPIOTx_handle);
+
+	GPIORx_handle.Pin = GPIO_PIN_7;
+	GPIORx_handle.Mode = GPIO_MODE_AF_PP;
+	GPIORx_handle.Pull = GPIO_NOPULL;
+	GPIORx_handle.Speed = GPIO_SPEED_FAST;
+	GPIORx_handle.Alternate = GPIO_AF7_USART1;
+
+	HAL_GPIO_Init(GPIOB, &GPIORx_handle);
+
+	__HAL_RCC_USART1_CLK_ENABLE()
+	;
+
+	uart_handle.Instance = USART1;
+	uart_handle.Init.BaudRate = 115200;
+	uart_handle.Init.WordLength = UART_WORDLENGTH_8B;
+	uart_handle.Init.StopBits = UART_STOPBITS_1;
+	uart_handle.Init.Parity = UART_PARITY_NONE;
+	uart_handle.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	uart_handle.Init.Mode = UART_MODE_TX_RX;
+
+	HAL_UART_Init(&uart_handle);
 }
